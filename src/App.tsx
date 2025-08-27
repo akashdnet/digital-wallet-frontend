d22bead084ee78f6dbe5b9d2ecf36f217e3af584
akashdnet <khondokerakashmi@gmail.com>
2025-08-27 11:09:08 +0600

    shadcn has been added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
 
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <div>
-      <h1 className="text font-bold underline">
-        Hello world!
-      </h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3000,
   },
